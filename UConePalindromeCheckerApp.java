1eb2283ef31a9a82bd4bd9f1e0966ee664697c44
Shreenithi-C <shreenithii3003@gmail.com>
2026-03-03 16:10:43 +0530

    Coded UC1 for Application Entry and Welcome Message

---
+++ UConePalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+package com.palindromeApp;
+
+public class UConePalindromeCheckerApp {
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to the Palindrome Management System");
+		System.out.println("Version 1.0");
+		System.out.println("System initialized succesfully");
+	}
+}
