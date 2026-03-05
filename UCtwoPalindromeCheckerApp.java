d961b2769957237792541a052beacd758634cd72
Shreenithi-C <shreenithii3003@gmail.com>
2026-03-05 11:44:02 +0530

    Coded UC2 to print a hardcoded plaindrome result

--- UCtwoPalindromeCheckerApp.java
+++ UCtwoPalindromeCheckerApp.java
@@ -3,23 +3,22 @@ package com.palindromeApp;
 public class UCtwoPalindromeCheckerApp {
     public static void main(String[] args) {
         String word = "malayalam"; // hardcoded string
+        int length = word.length();
+        boolean isPalindrome = true;
 
-        if (isPalindrome(word)) {
+        // Loop only till half
+        for (int i = 0; i < length / 2; i++) {
+            if (word.charAt(i) != word.charAt(length - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
             System.out.println(word + " is a palindrome.");
         } else {
             System.out.println(word + " is not a palindrome.");
         }
     }
-
-    public static boolean isPalindrome(String str) { 
-    	int length = str.length(); 
-    	for (int i = 0; i < length / 2; i++) 
-    	{ 
-    		if (str.charAt(i) != str.charAt(length - 1 - i)) 
-    		{ 
-    			return false; 
-    		}
-    	}
-    	return true;
-    }
 }
+
