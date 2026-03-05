fa701398925347d48c5eb28b08860d71edbaf01b
Shreenithi-C <shreenithii3003@gmail.com>
2026-03-05 12:14:04 +0530

    Coded UC8 to implement Linkedlist based Checker

--- UCeightPalindromeCheckerApp.java
+++ UCeightPalindromeCheckerApp.java
@@ -1,33 +1,36 @@
 package com.palindromeApp;
 
+/*
+@author developer
+@version 8.0
+*/
+
 import java.util.LinkedList;
 
-public class UCeightPalindromeCheckerApp{
+public class UCeightPalindromeCheckerApp {
     public static void main(String[] args) {
         String input = "level"; // test string
-
-        if (isPalindromeUsingLinkedList(input)) {
-            System.out.println(input + " is a palindrome.");
-        } else {
-            System.out.println(input + " is not a palindrome.");
-        }
-    }
-
-    public static boolean isPalindromeUsingLinkedList(String str) {
         LinkedList<Character> list = new LinkedList<>();
 
         // Add each character to the linked list
-        for (char c : str.toCharArray()) {
+        for (char c : input.toCharArray()) {
             list.add(c);
         }
+
+        boolean isPalindrome = true;
 
         // Compare front and rear until one or zero elements remain
         while (list.size() > 1) {
             if (!list.removeFirst().equals(list.removeLast())) {
-                return false; // mismatch found
+                isPalindrome = false;
+                break;
             }
         }
 
-        return true; // all matched
+        if (isPalindrome) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
     }
 }
