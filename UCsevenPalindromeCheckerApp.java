57b885124012495e547c90a9951de5be67942c4e
Shreenithi-C <shreenithii3003@gmail.com>
2026-03-05 12:09:30 +0530

    Coded UC7 to implement deque based checker

--- UCsevenPalindromeCheckerApp.java
+++ UCsevenPalindromeCheckerApp.java
@@ -1,34 +1,37 @@
 package com.palindromeApp;
 
+/*
+@author developer
+@version 7.0
+*/
+
 import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class UCsevenPalindromeCheckerApp {
     public static void main(String[] args) {
         String input = "refer"; // test string
-
-        if (isPalindromeUsingDeque(input)) {
-            System.out.println(input + " is a palindrome.");
-        } else {
-            System.out.println(input + " is not a palindrome.");
-        }
-    }
-
-    public static boolean isPalindromeUsingDeque(String str) {
         Deque<Character> deque = new ArrayDeque<>();
 
         // Add each character to the deque
-        for (char c : str.toCharArray()) {
+        for (char c : input.toCharArray()) {
             deque.add(c);
         }
+
+        boolean isPalindrome = true;
 
         // Compare front and rear until one or zero elements remain
         while (deque.size() > 1) {
             if (deque.removeFirst() != deque.removeLast()) {
-                return false; // mismatch found
+                isPalindrome = false;
+                break;
             }
         }
 
-        return true; // all matched
+        if (isPalindrome) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
     }
 }
